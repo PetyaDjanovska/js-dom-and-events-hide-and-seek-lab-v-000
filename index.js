24e14dc816b126fe36cf7495b6ceb7fa5d9f3f2f
Petya Djanovska <petya.djanovska@gmail.com>
2018-09-27 18:47:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ function getFirstSelector(selector) {
     return document.querySelector(selector);
 }
 
-function nestedTarget(){
+function nestedTarget() {
   return document.querySelector('#nested .target')
+}
+
+function increaseRankBy(n) {
+  let lis = document.querySelectorAll('ul.ranked-list li'))
 }
